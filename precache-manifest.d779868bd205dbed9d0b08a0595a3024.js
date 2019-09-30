2535cb589652ece726fe009602abfb1a5c5e6843
n.mikey.truong <n.mikey.truong@gmail.com>
2019-09-30 10:46:27 -0400

    Updates

---
+++ precache-manifest.d779868bd205dbed9d0b08a0595a3024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368f6eb6f93c449bcd50f08cc51326de",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "c77d9620b50e166760e8",
+    "url": "/clicky-game/static/css/main.05dde758.chunk.css"
+  },
+  {
+    "revision": "7601d659fc5fa8ab6413",
+    "url": "/clicky-game/static/js/2.e3513b8b.chunk.js"
+  },
+  {
+    "revision": "c77d9620b50e166760e8",
+    "url": "/clicky-game/static/js/main.5075f934.chunk.js"
+  },
+  {
+    "revision": "0c2b9b0e524a04a60c3c",
+    "url": "/clicky-game/static/js/runtime-main.98b6a1fe.js"
+  }
+]);
